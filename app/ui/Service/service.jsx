f8f2de248a0ecdb7d6214870b866f494cbe3b69a
Bhawesh <bhaweshjainskype@gmail.com>
2024-05-08 18:57:08 +0530

    Animate Test

--- app/ui/Service/service.jsx
+++ app/ui/Service/service.jsx
@@ -1,21 +1,27 @@
 import Image from "next/image";
+import { motion } from "framer-motion"
 
 const Service = ({ item }) => {
     return (
-        <div className="text-black p-3 border border-solid border-gray-300 rounded-lg bg-white flex flex-col items-center hover:scale-105 transition duration-300">
-            <div className="mt-5">
-                <Image
-                    src="/ic_trusted.png"
-                    alt=""
-                    width={50}
-                    height={50}
-                />
+        <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}  >
+            <div className="text-black p-3 border border-solid border-gray-300 rounded-lg bg-white flex flex-col items-center hover:scale-105 transition duration-300">
+                <div className="mt-5">
+                    <Image
+                        src="/ic_trusted.png"
+                        alt=""
+                        width={50}
+                        height={50}
+                    />
+                </div>
+
+                <h1 className="text-center font-bold mt-8 mb-4 text-lg">Service Name</h1>
+
+                <p className="text-center text-sm mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit quisquam, error voluptatum quas voluptate mollitia modi impedit pariatur, quidem vitae, dicta ab architecto quasi iste!</p>
             </div>
-
-            <h1 className="text-center font-bold mt-8 mb-4 text-lg">Service Name</h1>
-
-            <p className="text-center text-sm mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit quisquam, error voluptatum quas voluptate mollitia modi impedit pariatur, quidem vitae, dicta ab architecto quasi iste!</p>
-        </div>
+        </motion.div >
     );
 };
 
