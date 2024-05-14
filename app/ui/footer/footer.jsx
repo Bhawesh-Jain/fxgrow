41ce38d9e8a2ba13af5cfba6a580832ca8db32e7
Bhawesh Jain <bhaweshjain384@gmail.com>
2024-05-14 08:15:09 +0530

    Fixed Icon

--- app/ui/footer/footer.jsx
+++ app/ui/footer/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="bg-black text-white">
             <div>
                 <div className="flex flex-row content-center"><MdEmail size={30}/> <p>test@gmail.com</p></div>
-                <div className="flex flex-row content-center">< size={30}/> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore autem numquam consectetur ad.</p></div>
+                <div className="flex flex-row content-center"><MdLocationPin size={30}/> <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore autem numquam consectetur ad.</p></div>
                 <div className="flex flex-row content-center"><MdWhatsapp size={30}/> <p>9876543210</p></div>
             </div>
         </div>
