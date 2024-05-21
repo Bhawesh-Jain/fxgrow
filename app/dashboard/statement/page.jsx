ed9ebac21837639b1f0709c00576ce61420f3de9
Bhawesh <bhaweshjainskype@gmail.com>
2024-05-21 11:33:05 +0530

    statement

--- app/dashboard/statement/page.jsx
+++ app/dashboard/statement/page.jsx
@@ -1,4 +1,6 @@
 import { getSession } from "@/libs/actions";
+import Image from "next/image";
+import Link from "next/link";
 import { MdHistory } from "react-icons/md";
 
 const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001"
@@ -25,6 +27,28 @@ const Page = async () => {
 
    return (
       <div className="flex flex-col pb-5">
+
+         <div className=" flex flex-row justify-center gap-2 items-center">
+
+            <Link className="py-5" href="/">
+               <div className="flex flex-row gap-3  cursor-pointer select-none">
+                  <div className="color=#f53131">
+                     <Image
+                        src="/logo-icon.png"
+                        width={55}
+                        height={55}
+                        alt=""
+                        className="rounded-full"
+                     />
+                  </div>
+                  <div className="flex flex-col text-xl ml-2">
+                     <span className="text-black text-2xl font-bold">IForex</span>
+                     <span className="text-gray-900 font-semibold">Your Trading Partner</span>
+                  </div>
+               </div>
+            </Link>
+         </div>
+
          <div className="mx-3 my-8 text-xl flex flex-row justify-center gap-2 items-center">
             <MdHistory />
             <h2 className="font-bold">Statement</h2>
@@ -32,7 +56,7 @@ const Page = async () => {
          <div className="relative overflow-x-auto">
             <table className="w-full text-sm text-center text-nowrap whitespace-nowrap rtl:text-right text-black p-2 border border-gray-50">
                <thead className="text-xs uppercase">
-                  <tr className="">
+                  <tr className=" bg-black text-white">
                      <th scope="col" className="px-6 py-3 border-r">
                         Date
                      </th>
